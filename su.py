9456c18705ed90c27efe661f65acb2653eec4373
Kenan <kenankzmv@gmail.com>
2017-06-16 21:06:04 +0400

    Add files via upload

---
+++ su.py
@@ -0,0 +1,25 @@
+# -*- coding: cp1254 -*-
+import urllib.request
+import json
+from bs4 import BeautifulSoup
+import string
+ab_kodu = input("Abonent kodunuz: ")
+api_url = "http://data.e-gov.az/api/v1/IEGOVService.svc/GetDebtByAbonentCode/{}".format(ab_kodu)
+
+with urllib.request.urlopen(api_url) as url:
+    output = url.read().decode('utf-8')
+    data = json.loads(output)
+
+html = data['response']['htmlField']
+soup = BeautifulSoup(html, "html.parser")
+l = []
+
+for a in soup.find_all('b'):
+    l.append(a)
+b = str(a)
+b.replace("<b>", "a")
+
+kod = l[1]
+ad = l[3]
+borc = l[5]
+print("Abonentin kodu: {}\nAdý: {}\nBorc: {}".format(kod,ad,borc))
